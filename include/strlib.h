fb0097f72e38b2334b0222e72aa3b8941d03d9cf
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-04-05 20:37:20 +0530

    update umbrella header to allow exclusion and enforce dependency

--- include/strlib.h
+++ include/strlib.h
@@ -1,8 +1,28 @@
 #ifndef STRLIB_H_
 #define STRLIB_H_
 
-#include "sv.h"
-#include "str.h"
-#include "sb.h"
-
+#if !defined(NO_STR) && defined(NO_SV)
+#error "STR requires SV. Do not define NO_SV when using STR."
 #endif
+
+#if !defined(NO_SB) && defined(NO_STR)
+#error "SB requires STR. Do not define NO_STR when using SB."
+#endif
+
+#if !defined(NO_SB) && defined(NO_SV)
+#error "SB requires SV. Do not define NO_SV when using SB."
+#endif
+
+#ifndef NO_SV
+#include "sv.h"
+#endif
+
+#if !defined(NO_STR) && !defined(NO_SV)
+#include "str.h"
+#endif
+
+#if !defined(NO_SB) && !defined(NO_STR) && !defined(NO_SV)
+#include "sb.h"
+#endif
+
+#endif /* STRLIB_H_ */
